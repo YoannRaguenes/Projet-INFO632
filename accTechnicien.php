b448df068f83ed64568d9f97af970932cfc8d61c
YoannRaguenes <58259678+YoannRaguenes@users.noreply.github.com>
2020-05-04 14:06:02 +0200

    Add files via upload

--- accTechnicien.php
+++ accTechnicien.php
@@ -2,43 +2,43 @@
     <head>
 	
        <meta charset="utf-8">
-        <link rel="stylesheet" href="accTechnicien.css"  type="text/css" />
+        <link rel="stylesheet" href="accEtudiant.css"  type="text/css" />
     </head>
     <body>
 	
 		<div id="entete">
-			<h2 id="titre">Bienvenue sur la plateforme de commande de matériel</h2>
-
 		
 			<div id="cadreConnec">
 			Nom d'utilisateur
 			<p><a href="index.php">Se déconnecter</p></a>
-			</div>
-			
+			</div>		
 		
+			<div id="Bienvenue">
+			<h2>Bienvenue sur la plateforme de commande de matériel</h2>
+			</div>
+		
+			
+			
 		</div>
+		
 
 		<div id="main">
 			<div id="menu">
-			<input type="submit" id='submit' value='DEMANDES EN ATTENTE' >
-			<input type="submit" id='submit' value='DEMANDE TRAITEES' >
+			<input type="submit" id='submit'onclick=window.location.href='page_2.php' value='DEMANDES EN ATTENTE' >
+			<input type="submit" id='submit' value='DEMANDES TRAITEES' >
 			<input type="submit" id='submit' value='CONTACTER UN ENSEIGNANT' >
 			<input type="submit" id='submit' value='CONTACTER UN ELEVE' >
 			<input type="submit" id='submit' value='RESULTATS ENQUETE' >
 			
 			</div>
-			<div id="notifTchat">
-			Notifications tchat
-			</div>
 			
-			<div id="Carrousel">
-			Carrousel d'images ?
-			</div>
 			
 
 		<div id="contenu">
-		Version Technicien
-		<img src="images\logoPOPO.jpg" alt="" />
+			Version Etudiant
+			<div id="logopopo">
+			<img src="images\logoPOPO.jpg" alt="" />
+		</div>	
 		</div>
 		</div>
 
